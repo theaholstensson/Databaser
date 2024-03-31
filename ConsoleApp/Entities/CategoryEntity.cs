1acd700f2ab870918a7c515c18b080a053e1f34f
theaholstensson <thea.holstensson@hotmail.com>
2024-03-31 17:52:40 +0200

    Add project files.

---
+++ ConsoleApp/Entities/CategoryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Entities
+{
+    internal class CategoryEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = null!;
+
+    }
+}
