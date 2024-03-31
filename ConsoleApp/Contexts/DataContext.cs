6bd9f178564b94ddd61155dc5f4388c87bbd37e0
theaholstensson <thea.holstensson@hotmail.com>
2024-03-31 18:39:25 +0200

    Context and dependency injection made.

---
+++ ConsoleApp/Contexts/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Contexts
+{
+    internal class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddressEntity> Addresses { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<RoleEntity> Roles { get; set; }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+{
+    
+    services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Code\ConsoleApp\ConsoleApp\Data\database.mdf;Integrated Security=True;Connect Timeout=30"));
+
+}).Build();
